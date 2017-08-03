9ea26a0c1a397d8f970c924ad448094fc7e9ec5c
yevhen pyrohov <ypyrohov@softserveinc.com>
2017-08-03 13:08:13 +0300

    Download gdrive file

--- TaskManager/GDrive/GDriveDownloader.cs
+++ TaskManager/GDrive/GDriveDownloader.cs
@@ -1,44 +1,39 @@
 ﻿using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using System;
+using System.IO;
 using Google.Apis.Drive.v3.Data;
-
+using File = Google.Apis.Drive.v3.Data.File;
 namespace GanttMonoTracker
 {
     public class GDriveDownloader
     {
         public byte[] Download(GDriveCredentials cred, string fileId)
         {
-            //var service = new DriveService(new BaseClientService.Initializer()
-            //{
-            //    HttpClientInitializer = cred.Credential,
-            //    ApplicationName = "Gantt Mono Tracker",
-            //});
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = cred.Credential,
+                ApplicationName = "Gantt Mono Tracker",
+            });
 
-            //var l = service.Files.List();
-            //var list = l.Execute();
-            //File file1 = null;
-            //foreach (var f in list.Items)
-            //{
-            //    if (f.Description == "Gantt Mono Tracker project")
-            //    {
-            //        file1 = f;
-            //    }
-            //}
+            var l = service.Files.List();
+            var list = l.Execute();
+            File file1 = null;
+			foreach (var f in list.Files)
+            {
+                if (f.Description == "Gantt Mono Tracker project")
+                {
+                    file1 = f;
+                }
+            }
 
-            //var request = service.Files.Get(file1.Id);
-            //var file = request.Execute();
-
-            //if (!string.IsNullOrEmpty(file.DownloadUrl))
-            //{
-            //    var stream = service.HttpClient.GetStreamAsync(file.DownloadUrl);
-            //    using (var s = stream.Result)
-            //    {
-            //        return ReadToEnd(s);
-            //    }
-            //}
-
-            throw new InvalidOperationException();
+			//https://developers.google.com/drive/v3/web/manage-downloads
+            var request = service.Files.Get(file1.Id);
+			using (var s = new MemoryStream())
+			{
+				request.Download(s);
+				return s.ToArray();
+			}
         }
 
         public static byte[] ReadToEnd(System.IO.Stream stream)
